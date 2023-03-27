43c1f3d7a57c862f5556b91999989be33ce5c066
Alexssandro <22282169@CFP127ALUNO.LOCAL>
2023-03-27 16:58:23 -0300

    api finalizada

--- index.js
+++ index.js
@@ -15,8 +15,6 @@
 */
 
 //import das funções
-const alunos = require('./module/alunos.js')
-const cursos = require('./module/cursos.js')
 const funcs = require('./module/funcs.js')
 
 //Import das dependências do projeto
@@ -39,22 +37,119 @@ app.use((request, response, next) => {
     next()
 })
 
-//EndPoint que chama a função que retorna os cursos
+//EndPoint que chama a função que retorna todos os cursos
 app.get('/v1/lion-school/cursos', cors(), async (request, response, next) => {
     
-    let cursos = funcs.getListaCursos(cursos)
+    let cursos = funcs.getListaCursos()
     let statusCode;
     let dadosCurso = {}
 
     if (cursos) {
         statusCode = 200
-        dadoscurso = cursos
+        dadosCurso = cursos
     } else {
         statusCode = 404
     }
 
     response.status(statusCode)
     response.json(dadosCurso)
+
+})
+
+//Endpoint que chama a função que retorna todos os alunos
+app.get('/v1/lion-school/alunos', cors(), async (request, response, next) => {
+    
+    let alunos = funcs.getListaAlunos()
+    let statusCode;
+    let dadosAluno = {}
+
+    if (alunos) {
+        statusCode = 200
+        dadosAluno = alunos
+    } else {
+        statusCode = 404
+    }
+
+    response.status(statusCode)
+    response.json(dadosAluno)
+
+})
+
+//EndPoint que chama a função que retorna as informações de um aluno usando a matricula como parametro
+app.get('/v1/lion-school/alunos/:matricula', cors(), async (request, response, next) => {
+    
+    let NumMatricula = request.params.matricula
+    let statusCode;
+    let dadosAluno = {}
+
+    if (NumMatricula == '' || NumMatricula == undefined || NumMatricula.length > 11 || isNaN(NumMatricula)) {
+        statusCode = 400
+        dadosAluno.message = "Não foi possível processar, pois os dados de entrada (matricula) que foi enviadoo não corresponde ao exigido. Confira o valor, pois não pode ser vazio, precisam ser números e ter 11 dígitos."
+    } else {
+        let aluno = funcs.getAlunosMatricula(NumMatricula)
+        if (aluno) {
+            statusCode = 200
+            dadosAluno = aluno
+        } else {
+            statusCode = 404
+        }
+    }
+    
+
+    response.status(statusCode)
+    response.json(dadosAluno)
+
+})
+
+//EndPoint que chama a função que retorna todos os alunos matriculados em determinada materia
+app.get('/v1/lion-school/aluno', cors(), async (request, response, next) => {
+    
+    let cursos = request.query.curso
+    let statusCode;
+    let dadosAlunos = {}
+
+    if (cursos == '' || cursos == undefined || cursos.length > 3 || !isNaN(cursos)) {
+        statusCode = 400
+        dadosAlunos.message = "Não foi possível processar, pois os dados de entrada (curso) que foi enviado não corresponde ao exigido. Confira o valor, pois não pode ser vazio, precisam ser números e ter no máximo 3 dígitos."
+    } else {
+        let curso = funcs.getAlunosCurso(cursos)
+        if (curso) {
+            statusCode = 200
+            dadosAlunos = curso
+        } else {
+            statusCode = 404
+        }
+    }
+    
+
+    response.status(statusCode)
+    response.json(dadosAlunos)
+
+})
+
+//Endpoint que chama a função que retorna todos os alunos e suas respectivas disciplinas levando o status como parâmetro
+app.get('/v1/lion-school/alunoss', cors(), async (request, response, next) => {
+    
+    let status = request.query.status
+    let statusCode;
+    let dadosStatus = {}
+
+    if (status == '' || status == undefined || status.length > 9|| !isNaN(status)) {
+        statusCode = 400
+        dadosStatus.message = "Não foi possível processar, pois os dados de entrada (curso) que foi enviado não corresponde ao exigido. Confira o valor, pois não pode ser vazio, precisam ser números e ter no máximo 3 dígitos."
+    } else {
+        let statusAluno = funcs.getStatus(status)
+        if (statusAluno) {
+            statusCode = 200
+            dadosStatus = statusAluno
+        } else {
+            statusCode = 404
+        }
+    }
+    
+
+    response.status(statusCode)
+    response.json(dadosStatus)
 
 })
 
--- module/funcs.js
+++ module/funcs.js
@@ -4,29 +4,152 @@
     Data: 24/03/23
     Versão: 1.0
 */
-
 const cursos = require('./cursos.js')
+const alunos = require('./alunos.js')
+const { json } = require('body-parser')
 
-const getListaCursos = (lista) => {
+const getListaCursos = () => {
+    let listaCursos = {}
     let arrayCursos = []
 
-    lista.cursos.forEach((curso) => {
-        
-        let listaCursos = {}    
+    listaCursos.cursos = arrayCursos
 
-        listaCursos.sigla = curso.sigla
-        listaCursos.icone = curso.icone
-        
-        arrayCursos.push(listaCursos)
-        
-    });
+    cursos.cursos.forEach((curso) => {
 
-    return arrayCursos
+        let JSONCursos = {}
+
+        JSONCursos.sigla = curso.sigla
+        JSONCursos.icone = curso.icone
+
+        arrayCursos.push(JSONCursos)
+
+    })
+
+    return listaCursos
 
 }
 
-// console.log(getListaCursos(cursos))
+// console.log(getListaCursos())
+
+const getListaAlunos = () => {
+    let listaAlunos = {}
+    let arrayAlunos = []
+
+    listaAlunos.alunos = arrayAlunos
+
+    alunos.alunos.forEach((aluno) => {
+
+        let JSONAlunos = {}
+
+        JSONAlunos.foto = aluno.foto
+        JSONAlunos.nome = aluno.nome
+        JSONAlunos.matricula = aluno.matricula
+        JSONAlunos.sexo = aluno.sexo
+        aluno.curso.forEach((curso) => {
+            JSONAlunos.curso = curso.nome
+        })
+
+        arrayAlunos.push(JSONAlunos)
+
+    })
+
+    return listaAlunos
+}
+
+// console.log(getListaAlunos())
+
+const getAlunosMatricula = (matricula) => {
+    let listaAlunos = {}
+    let arrayAlunos = []
+
+    listaAlunos.aluno = arrayAlunos
+
+    alunos.alunos.forEach((aluno) => {
+
+        if (aluno.matricula == matricula && aluno.matricula != undefined && !isNaN(aluno.matricula) && aluno.matricula.length <= 11) {
+            let JSONAlunos = {}
+
+            JSONAlunos.foto = aluno.foto
+            JSONAlunos.nome = aluno.nome
+            JSONAlunos.matricula = aluno.matricula
+            JSONAlunos.sexo = aluno.sexo
+            aluno.curso.forEach((curso) => {
+                JSONAlunos.curso = curso.nome
+            })
+
+            arrayAlunos.push(JSONAlunos)
+        }
+    })
+
+    return listaAlunos
+}
+
+// console.log(getAlunosMatricula(20151001001))
+
+const getAlunosCurso = (curso) => {
+    let listaAlunos = {}
+    let arrayAlunos = []
+
+    listaAlunos.aluno = arrayAlunos
+
+    alunos.alunos.forEach((aluno) => {
+
+        aluno.curso.forEach((nomeCurso) => {
+
+            if (nomeCurso.sigla.toUpperCase() == curso.toUpperCase()) {
+                let JSONAlunos = {}
+
+                JSONAlunos.foto = aluno.foto
+                JSONAlunos.nome = aluno.nome
+                arrayAlunos.push(JSONAlunos)
+
+            }
+
+        })
+
+    })
+
+    return listaAlunos
+}
+
+// console.log(getAlunosCurso("RDS"))
+
+const getStatus = (status) => {
+    let listaAlunos = {}
+    let arrayAlunos = []
+
+    listaAlunos.aluno = arrayAlunos
+
+    alunos.alunos.forEach((aluno) => {
+
+        aluno.curso.forEach((curso) => {
+
+            curso.disciplinas.forEach((disciplina) => {
+
+                if (disciplina.status.toUpperCase() == status.toUpperCase()) {
+                    let JSONAlunos = {}
+
+                    JSONAlunos.aluno = aluno.nome
+                    JSONAlunos.curso = curso.sigla
+                    JSONAlunos.disciplina = disciplina.nome
+                    JSONAlunos.status = disciplina.status
+                    arrayAlunos.push(JSONAlunos)
+                }
+
+
+            })
+        })
+    })
+
+    return listaAlunos
+}
+
+// console.log(getStatus("Aprovado"))
 
 module.exports = {
-    getListaCursos
+    getListaCursos,
+    getListaAlunos,
+    getAlunosMatricula,
+    getAlunosCurso,
+    getStatus
 }
